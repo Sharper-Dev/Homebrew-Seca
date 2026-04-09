e74f793853dd54e0e685cd3cbcf3c08bee975492
Sharper Dev <guilherme.senadeaguiar626@gmail.com>
2026-04-08 21:34:25 -0300

    Organization

--- tools/3dsx_buildscript.bat
+++ tools/3dsx_buildscript.bat
@@ -1,2 +1,2 @@
 3dstool -cvtf romfs "../romfs.bin" --romfs-dir "../romfs"
-3dsxtool "../build/lpp-3ds.elf" "../build/lpp-3ds.3dsx" --romfs="../romfs.bin" --smdh="../build/lpp-3ds.smdh"
+3dsxtool "../build assets/source.elf" "../build/lpp-3ds.3dsx" --romfs="../romfs.bin" --smdh="../build assets/source.smdh"
